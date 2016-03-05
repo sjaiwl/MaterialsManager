ba93cd0eb00ea30eb78a18847542e864ffcb555d
sjaiwl <761034053@qq.com>
2016-03-05 19:11:54 +0800

    first add

---
+++ MaterialsManagerApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MaterialsManagerApp
+//
+//  Created by 王林 on 16/1/17.
+//  Copyright © 2016年 sjaiwl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
